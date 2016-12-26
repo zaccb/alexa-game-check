497d3fd27f83e9254053c15143b6dea2f0226fbc
Zach Baker <zach.baker@slalom.com>
2016-12-25 21:21:42 -0800

    Moved target directory for build aratifacts to dist/

--- script.archive.sh
+++ script.archive.sh
@@ -4,4 +4,4 @@ NOW=$(date +"%Y%m%d-%H%M")
 
 cd ~/source/alexa-game-check/src
 
-zip -r archive$NOW.zip index.js AlexaSkill.js cities.js node_modules
+zip -r ~/source/alexa-game-check/dist/archive$NOW.zip index.js AlexaSkill.js cities.js node_modules
